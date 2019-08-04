c24dfada6fea3343f318771a5894e8ab9b83345c
Ramesh <ramesh@rsageventures.com>
2019-08-04 11:13:50 +0530

    HystrixCircuitBreaker implemented

--- movie-catalog-service/src/main/java/com/rs/app/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/rs/app/controller/MovieCatalogController.java
@@ -1,38 +1,52 @@
 package com.rs.app.controller;
 
+import java.util.concurrent.TimeUnit;
 
-import com.rs.app.model.Movie;
-import com.rs.app.service.MovieCatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.app.model.Movie;
+import com.rs.app.service.MovieCatalogService;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
- * Use Dashboard URL
- * http://localhost:8082/actuator/hystrix.stream
+ * Hystrix Dashboard: http://localhost:8082/hystrix
+ * Hystrix Stream: http://localhost:8082/actuator/hystrix.stream
  */
 @RestController
 @RequestMapping(MovieCatalogController.RESOURCE_URI)
+@Slf4j
 public class MovieCatalogController {
-    protected static final String RESOURCE_URI = "/movie/catalog";
+	protected static final String RESOURCE_URI = "/movie/catalog";
 
-    @Autowired
-    private MovieCatalogService movieCatalogService;
+	@Autowired
+	private MovieCatalogService movieCatalogService;
 
-    @GetMapping("/all")
-    public Iterable<Movie> getAvailableMovies() {
-        return movieCatalogService.getAvailableMovies();
-    }
+	@GetMapping("/all")
+	public Iterable<Movie> getAvailableMovies() {
+		return movieCatalogService.getAvailableMovies();
+	}
 
-    @GetMapping("/id/{id}")
-    public Movie getMovie(@PathVariable long id) {
-        return movieCatalogService.getMovieById(id);
-    }
+	@GetMapping("/id/{id}")
+	public Movie getMovie(@PathVariable long id) {
+		// sleep(3, TimeUnit.SECONDS);
+		return movieCatalogService.getMovieById(id);
+	}
 
-    @GetMapping("/name/{name}")
-    public Movie getMovie(@PathVariable String name) {
-        return movieCatalogService.getMovieByName(name);
-    }
+	@GetMapping("/name/{name}")
+	public Movie getMovie(@PathVariable String name) {
+		return movieCatalogService.getMovieByName(name);
+	}
+
+	private static void sleep(long timeout, TimeUnit timeUnit) {
+		try {
+			timeUnit.sleep(timeout);
+		} catch (InterruptedException ie) {
+			log.error(ie.getMessage());
+		}
+	}
 }
--- movie-info-service/src/main/java/com/rs/app/controller/MovieInfoController.java
+++ movie-info-service/src/main/java/com/rs/app/controller/MovieInfoController.java
@@ -1,46 +1,63 @@
 package com.rs.app.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.rs.app.model.Movie;
-import com.rs.app.service.MovieInfoService;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.rs.app.model.Movie;
+import com.rs.app.service.MovieInfoService;
 
 @RestController
 @RequestMapping(MovieInfoController.RESOURCE_URI)
 public class MovieInfoController {
-    protected static final String RESOURCE_URI = "/movie/info";
+	protected static final String RESOURCE_URI = "/movie/info";
 
-    @Autowired
-    private MovieInfoService movieInfoService;
+	@Autowired
+	private MovieInfoService movieInfoService;
+// Reference: https://github.com/Netflix/Hystrix/wiki/Configuration 
+	@GetMapping("/id/{id}")
+	@HystrixCommand(
+			commandKey = "getMovieById", 
+			fallbackMethod = "getMovieInfoByIdFallback",
+			commandProperties = {
+					@HystrixProperty(name = "execution.timeout.enabled", value = "true"),
+					@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+					@HystrixProperty(name = "execution.isolation.thread.interruptOnTimeout", value = "true"),
+					@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "6"),
+					@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+					@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+			},
+			threadPoolKey = "getMovieByIdThreadPool",
+			threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "5"),
+					@HystrixProperty(name = "maxQueueSize", value = "10"),
+					@HystrixProperty(name = "keepAliveTimeMinutes", value = "1")
+			})
+	public ResponseEntity<Movie> getMovieById(@PathVariable("id") long id, HttpServletRequest httpServletRequest) {
+		Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			System.out.println(headerName + " <========> " + httpServletRequest.getHeader(headerName));
+		}
+		return ResponseEntity.ok(movieInfoService.getMovie(id));
+	}
 
+	@GetMapping("/name/{name}")
+	public Movie getMovieByName(@PathVariable("name") String name) {
+		return movieInfoService.getMovie(name);
+	}
 
-    @GetMapping("/id/{id}")
-    @HystrixCommand(commandKey = "movieById", fallbackMethod = "fallbackError")
-    public ResponseEntity<Movie> getMovieById(@PathVariable("id") long id, HttpServletRequest httpServletRequest) {
-        Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + " <========> " + httpServletRequest.getHeader(headerName));
-        }
-        return ResponseEntity.ok(movieInfoService.getMovie(id));
-    }
-
-    @GetMapping("/name/{name}")
-    public Movie getMovieByName(@PathVariable("name") String name) {
-        return movieInfoService.getMovie(name);
-    }
-
-    public ResponseEntity<Movie> fallbackError(long id, HttpServletRequest req) {
-        //String errorMessage = String.format("Unable to get moving having id = %s  \n RequestURI: " + req.getRequestURI());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    }
+	public ResponseEntity<Movie> getMovieInfoByIdFallback(long id, HttpServletRequest req) {
+		Movie movie = new Movie();
+		return ResponseEntity.ok(movie);
+	}
 }
